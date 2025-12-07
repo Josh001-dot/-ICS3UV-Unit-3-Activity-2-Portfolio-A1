2a35cf1c4ec323b9ad76d5a84c78bfd7dd96d031
default <joshua.adeyemi@stu.ocsb.ca>
2025-12-07 22:30:42 +0000

    commit message

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 /**
  * @author Joshua Adeyemi
  * @version 1.0.0
- * @date 2025-12-05
+ * @date 2025-12-07
  * @fileoverview This program asks for the user's name and
  * displays the Happy Birthday song with their name.
  */
 
 // INPUT
-const userName: string = prompt("Enter your first name:") ?? "Friend";
+let userName: string | null = prompt("Enter your first name:");
+
+if (userName === null || userName === "") {
+  userName = "Friend";
+}
 
 // OUTPUT
 console.log("Happy Birthday to you");
